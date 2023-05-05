01f7ddea1be7dd304c66cf571333ec77db6286e7
Arturo Ramirez <demonoid@outlook.jp>
2023-05-04 20:43:04 -0600

    Añadiendo funcionalidad a la etapa 2 de proyecto

---
+++ #Ejercicios en JavaCC/ejercicios/src/ejemplo_esquemas2/esquema2.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package ejemplo_esquemas2;
+
+/**
+ * @author Alush
+ *
+ */
+public class esquema2 {
+
+	/**
+	 * 
+	 */
+	public esquema2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
